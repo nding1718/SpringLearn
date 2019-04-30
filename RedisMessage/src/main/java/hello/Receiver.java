d8ad823d0937129599f33c7108891aed02033684
nding1718 <nding1718@gmail.com>
2019-04-29 22:09:31 -0700

    redismessage collection not finished

---
+++ RedisMessage/src/main/java/hello/Receiver.java
@@ -0,0 +1,20 @@
+package hello;
+
+import java.util.concurrent.CountDownLatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Receiver {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+	private CountDownLatch latch;
+
+	@Autowired
+	public Receiver(CountDownLatch latch) {
+		this.latch = latch;
+	}
+	public void receiveMessage(String message) {
+		LOGGER.info("Receive <" + message + ">");
+		latch.countDown();
+	}
+}
